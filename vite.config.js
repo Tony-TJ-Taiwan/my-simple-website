3850afd809532279dc83f6f545d6ec62d232d9bd
Tony Wang <aaa.com>
2026-01-01 23:14:43 +0800

    fix: 修正部署路徑

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-simple-website/', // 例如 '/run-nutri/'
+  base: '/my-simple-website/', // 這裡必須跟你的 GitHub 儲存庫名稱完全一致
 })
